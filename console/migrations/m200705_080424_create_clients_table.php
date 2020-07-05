a7efac8e79d261b8cd91b5144f9f468b3ab78e56
Amal Izmailov <izmailov.a.g@uremont.com>
2020-07-05 12:07:08 +0300

    fix clients table. indexes added

--- console/migrations/m200705_080424_create_clients_table.php
+++ console/migrations/m200705_080424_create_clients_table.php
@@ -23,6 +23,10 @@ class m200705_080424_create_clients_table extends Migration
             'cost'          => $this->decimal(10, 2)->null()->comment('Стоимость'),
             'first_payment' => $this->decimal(10, 2)->null()->comment('Первый взнос'),
         ]);
+
+        $this->createIndex('idx_clients_category_id', '{{%clients}}', 'category_id');
+        $this->createIndex('idx_clients_cost', '{{%clients}}', 'cost');
+        $this->createIndex('idx_clients_first_payment', '{{%clients}}', 'first_payment');
     }
 
     /**
